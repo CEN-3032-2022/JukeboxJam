b241faafe66de14929563bb5b4e8a5f4a71e7b03
Jonathan Bailey <jessebailey99@gmail.com>
2022-04-02 16:34:44 -0500

    Add project files.

---
+++ JukeboxClient/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JukeboxClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            FixMenuAlignment();
+            InitializeComponent();
+        }
+
+        private void FixMenuAlignment()
+        {
+            var ifLeft = SystemParameters.MenuDropAlignment;
+            if (ifLeft)
+            {
+                // change to false
+                var t = typeof(SystemParameters);
+                var field = t.GetField("_menuDropAlignment", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static);
+                field.SetValue(null, false);
+                ifLeft = SystemParameters.MenuDropAlignment;
+            }
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
