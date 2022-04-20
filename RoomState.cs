db4efb3e5ef6d045171a5f4dc308520e0d9e2eea
Elizabeth Uebele <geu1@students.uwf.edu>
2022-04-20 15:49:36 -0500

    Server code for posting and getting room state

--- RoomState.cs
+++ RoomState.cs
@@ -1,19 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
-
-namespace JukeboxClient
+﻿namespace Jukebox
 {
     public class RoomState
     {
-        [JsonProperty("position")]
         public TimeSpan Position { get; set; }
-        [JsonProperty("songIndex")]
         public int SongIndex { get; set; }
-        [JsonProperty("songState")]
         public int SongState { get; set; }
     }
 }
