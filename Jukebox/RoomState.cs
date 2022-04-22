620664bc4982361d0cfebc29c1100be7537c1df4
KevinCarff <89530008+KevinCarff@users.noreply.github.com>
2022-04-22 16:49:52 -0500

    Added roomstate functionality

---
+++ Jukebox/RoomState.cs
@@ -0,0 +1,9 @@
+namespace Jukebox
+{
+    public class RoomState
+    {
+        public TimeSpan Position { get; set; }
+        public int SongIndex { get; set; }
+        public int SongState { get; set; }
+    }
+}
