745f3b8ae7f6556e6764bec38f170810b8a06cd8
KevinCarff <89530008+KevinCarff@users.noreply.github.com>
2022-04-22 16:50:50 -0500

    Added RoomState Functionality

---
+++ Jukebox/RoomStateController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jukebox.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RoomStateController : ControllerBase
+    {
+
+        RoomState roomState = new RoomState();
+        bool songPlaying = false;
+
+        [HttpGet("/getRoomState")]
+        public async Task<ActionResult> Get()
+        {
+            return Ok(roomState);
+        }
+
+        [HttpPost("/postRoomState")]
+
+        public async Task<ActionResult> Send(RoomState room)
+        {
+            roomState = room;
+            return Ok();
+        }
+
+    }
+}
